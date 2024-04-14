8e2045cfc07ce3b150591c440ba35d60b1eb1877
shivamjha1 <shivam.21b0101002@abes.ac.in>
2024-04-15 00:41:04 +0530

    nd

--- review/forms.py
+++ review/forms.py
@@ -1,6 +1,22 @@
 from django import forms
 
-class ReviewForm(forms.Form):
-    user_name=forms.CharField(max_length=100,label="Your Name")
-    review_text=forms.CharField(label="Your Review",max_length=200,widget=forms.Textarea)
-    rating=forms.IntegerField(min_value=1,max_value=5)
+from .models import Review
+# class ReviewForm(forms.Form):
+#     user_name=forms.CharField(max_length=100,label="Your Name")
+#     review_text=forms.CharField(label="Your Review",max_length=200,widget=forms.Textarea)
+#     rating=forms.IntegerField(min_value=1,max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model=Review
+        fields="__all__"
+        #exclude=[]
+        labels={
+            "user_name":"Your Name",
+            "review_text":"Your Feedback",
+            "rating":"Your Rating"
+        }
+        error_messages={
+            "user_name":{
+                "required":"Your name must not be empty"}
+        }
--- review/views.py
+++ review/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 from .forms import ReviewForm
-from .models import Review
+#from .models import Review
 
 def review(request):
     if request.method=='POST':
         form=ReviewForm(request.POST)
         if form.is_valid():
-            x=form.cleaned_data
-            rev=Review(user_name=x['user_name'],review_text=x['review_text'],rating=x['rating'])
-            rev.save()
+            # x=form.cleaned_data
+            # rev=Review(user_name=x['user_name'],review_text=x['review_text'],rating=x['rating'])
+            # rev.save()
+            form.save()
             return HttpResponseRedirect('/thank-you')
     else:
         form=ReviewForm()
