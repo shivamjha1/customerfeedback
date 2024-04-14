bfc701ffb6a7dd15d314f863681eaf75e267a6f7
shivamjha1 <shivam.21b0101002@abes.ac.in>
2024-04-14 19:41:00 +0530

    d

--- review/views.py
+++ review/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
+from .forms import ReviewForm
+
 def review(request):
-    return HttpResponse("Hello")
+    if request.method=='POST':
+        return HttpResponseRedirect('/thank-you')
+    form=ReviewForm()
+    return render(request,"review/review.html",{
+        "form":form
+    })
+
+def thank_you(request):
+    return render(request,"review/thank-you.html")
