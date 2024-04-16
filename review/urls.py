39dfe837585903b165801346d71d351bd726d6bc
shivamjha1 <shivam.21b0101002@abes.ac.in>
2024-04-16 13:07:35 +0530

    x

--- review/urls.py
+++ review/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 urlpatterns = [
     path("",views.ReviewView.as_view()),
-    path("thank-you",views.thank_you)
+    path("thank-you",views.Thank_youView.as_view()),
+    path("review",views.Review_list.as_view()),
+    path("review/<slug:username>",views.Re)
 ]
--- review/views.py
+++ review/views.py
@@ -1,9 +1,12 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 from .forms import ReviewForm
+from .models import Review
 #from .models import Review
 from django.views import View
+from django.views.generic.base import TemplateView
 # def review(request):
 #     if request.method=='POST':
 #         form=ReviewForm(request.POST)
@@ -36,5 +39,22 @@ class ReviewView(View):
         "form":form
     })
 
-def thank_you(request):
-    return render(request,"review/thank-you.html")
+
+class Thank_youView(TemplateView):
+    template_name="review/thank-you.html"
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        return super().get_context_data(**kwargs)
+class Review_list(TemplateView):
+    template_name="review/review_list.html"
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["reviews"] = Review.objects.all()
+        return context
+    
+class Single_Review(TemplateView):
+    template_name="review/single_review.html"
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context[""] = 
+        return context
+    
